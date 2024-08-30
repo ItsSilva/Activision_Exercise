ba7c3522c26e2d6e4827c07a9828512ac17176fe
Juan Silva <juanse909@hotmail.com>
2024-08-30 01:26:11 -0500

    banner component working

--- public/src/components/banner/banner.js
+++ public/src/components/banner/banner.js
@@ -1,7 +1,7 @@
 class Banner extends HTMLElement {
 
     static get observedAttributes(){
-
+        return ['img', 'alt', 'logo', 'description', 'button00', 'button01'];
     }
     constructor(){
         super();
@@ -17,6 +17,26 @@ class Banner extends HTMLElement {
     }
     render(){
         this.shadowRoot.innerHTML = `
+<link rel="stylesheet" href="./src/components/banner/banner.css">
+<section>
+    <div class='hero-img'>
+        <img src="${this.img}" alt="${this.alt}" />
+        <div class='logo-buttons-container'>
+            <div class='logo-img'>
+                <img src="${this.logo}" alt="${this.alt}" />
+                <p>${this.description}</p>
+            </div>
+            <div class="button">
+                <div class="button-register">
+                    <a href="index.html">${this.button00}</a>
+                </div>
+                <div class="button-login">
+                    <a href="index.html">${this.button01}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
         `;
     }
 }
--- public/src/indexGrandpa.js
+++ public/src/indexGrandpa.js
@@ -11,6 +11,16 @@ class Container extends HTMLElement {
     render(){
         this.shadowRoot.innerHTML = `
         <navbar-component></navbar-component>
+
+        <banner-component
+        img = "./src/assets/img/94f5e08d4f2ef3e70d7aa0f9fc3de5b2.jpg"
+        alt = "Call of Duty Black Ops 6 character"
+        logo = "./src/assets/img/BlackOps6_Logo_White_BO6.webp"
+        description = "Forced to go rogue. Hunted form Within. This is Black Ops 6."
+        button00 = "Pre-Order Now"
+        button01 = "Visit site"
+        ></banner-component>
+
          `
     }
 }
